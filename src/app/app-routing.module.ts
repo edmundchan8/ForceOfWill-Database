8f0974accea2dfb35b90f30fa2c63522e3916c06
unknown <pokedev8@gmail.com>
2021-01-07 17:54:26 -0800

    added basic routing to code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdvancedSearchComponent } from './components/Prerelease/advanced-search/advanced-search.component';
+import { CardComponent } from './components/Prerelease/card/card.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'card', component: CardComponent },
+  { path: 'advanced_search', component: AdvancedSearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ CardComponent, AdvancedSearchComponent ]
